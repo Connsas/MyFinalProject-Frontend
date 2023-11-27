866070f0eee3f1adccdd364726b7b5d60bad5b87
İsmail <ismailoner1403@gmail.com>
2023-11-27 16:38:16 +0300

    17. Gün bitti

--- src/app/componenets/product/product.component.ts
+++ src/app/componenets/product/product.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product';
+
+import { ProductResponseModel } from '../../models/productResponseModel';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -9,7 +12,20 @@ import { Product } from '../../models/product';
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
 
   products: Product[] = [];
+  dataLoaded = false
+
+  getProducts() {
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true
+    });
+  }
 }
