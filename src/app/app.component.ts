f1679f147b56f49f0220a0f31492ab422542e09d
İsmail <ismailoner1403@gmail.com>
2023-11-27 14:12:34 +0300

    17.Gün

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NaviComponent } from "./componenets/navi/navi.component";
+import { CategoryComponent } from "./componenets/category/category.component";
+import { ProductComponent } from "./componenets/product/product.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, NaviComponent, CategoryComponent, ProductComponent]
 })
 export class AppComponent {
   title = 'Northwind';
-}
+  user: string = "İsmail Öner"
+  }
